adcedea0844c319367052aa79c12ecab76651a7d
vidyanshu kushawaha <vidyanshukushawaha@gmail.com>
2025-09-28 21:36:01 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages,setup
+from typing import List
+def get_requirements()->List[str]:
+        """
+        This function return list of requirements 
+        """
+        requirement_lst:List[str]=[]
+        try:
+                with open('requirements.txt','r') as file:
+                        # Read lines from the line
+                        lines=file.readlines()
+                        ## Process Each line
+                        for line in lines:
+                                requirement=line.strip()
+                                ## remove line and -e .
+                                if requirement and requirement!='-e .':
+                                        requirement_lst.append(requirement)
+        except FileNotFoundError:
+                print("requirements.txt file not found")
+        return requirement_lst
+print(get_requirements())
