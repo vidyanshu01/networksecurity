9640a7b9b70b4fcd6e0b4f9a92990c9906f1f85d
vidyanshu kushawaha <vidyanshukushawaha@gmail.com>
2025-10-24 10:19:50 +0530

    final changes to deployment

--- app.py
+++ app.py
@@ -100,7 +100,6 @@ async def predict_route(request: Request,file:UploadFile=File(...)):
         raise NetworkSecurityException(e, sys)
 
 if __name__=="__main__":
-        # app_run(app,host="0.0.0.0",port=8000)
         app_run(app,host="0.0.0.0",port=8080)
 
 
